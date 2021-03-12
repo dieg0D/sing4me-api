259986d63c4617a0f8d629ec6201f6fc399a9071
dieg0d <diegodi05@hotmail.com>
2021-03-12 01:30:51 -0300

    add ssl true

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,5 +4,6 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + process.env.ENTITIES],
+  ssl: true,
   synchronize: true,
 };
