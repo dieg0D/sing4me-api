8b65360aa618b1c6b3cdbc6cf7c1d8776a9f6a85
dieg0d <diegodi05@hotmail.com>
2021-03-12 21:31:57 -0300

    change typeormconfig

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -4,6 +4,9 @@ export const typeOrmConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   url: process.env.DATABASE_URL,
   entities: [__dirname + process.env.ENTITIES],
-  ssl: { rejectUnauthorized: false },
   synchronize: true,
+  logging: true,
+  extra: {
+    ssl: true,
+  },
 };
