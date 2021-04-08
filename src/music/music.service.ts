5df8977dd396a32e0e145104a18bea55ece093fd
dieg0d <diegodi05@hotmail.com>
2021-04-08 08:57:53 -0300

    Tratando possiveis erros

--- src/music/music.service.ts
+++ src/music/music.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { InternalServerErrorException, Injectable } from '@nestjs/common';
 import * as ytdl from 'ytdl-core';
 import * as ytsr from 'ytsr';
 
@@ -7,15 +7,38 @@ import { Response } from 'express';
 @Injectable()
 export class MusicService {
   findOne(id: string, res: Response) {
-    res.header('Content-Disposition', `attachment; filename="${id}.mp4"`);
+    try {
+      if (!id) {
+        throw new InternalServerErrorException(
+          'Ocorreu um erro tente novamente!',
+        );
+      }
+      res.header('Content-Disposition', `attachment; filename="${id}.mp4"`);
 
-    return ytdl(`https://www.youtube.com/watch?v=${id}`, {
-      filter: 'videoandaudio',
-    }).pipe(res);
+      return ytdl(`https://www.youtube.com/watch?v=${id}`, {
+        filter: 'videoandaudio',
+      }).pipe(res);
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Ocorreu um erro tente novamente!',
+      );
+    }
   }
 
   async search(title: string) {
-    const searchResults = await ytsr(title, { limit: 10 });
-    return searchResults;
+    try {
+      if (!title) {
+        throw new InternalServerErrorException(
+          'Ocorreu um erro tente novamente!',
+        );
+      }
+
+      const searchResults = await ytsr(title, { limit: 20 });
+      return searchResults;
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Ocorreu um erro tente novamente!',
+      );
+    }
   }
 }
